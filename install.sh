39aaff8933beae2e5ba0d198307e600f15a66ee3
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-10 22:58:42 +0900

    Add icons.

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 scmdir=$(pkg-config --variable=uim_scmdir uim)
 pixmapsdir=$(pkg-config --variable=uim_datadir uim)/pixmaps
 cp japan-util.scm japan-util-custom.scm "$scmdir"
-#cp pixmaps/japan-util.png pixmaps/japan-util_dark_background.png "$pixmapsdir"
-#cp pixmaps/japan-util.svg pixmaps/japan-util_dark_background.svg "$pixmapsdir"
+cp pixmaps/japan-util.png "$pixmapsdir"
+cp pixmaps/japan-util.svg "$pixmapsdir"
 uim-module-manager --register japan-util
