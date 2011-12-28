1bbd7a7a0f21b65ea9f142c0e839109636d296a8
KIHARA Hideto <deton@m1.interq.or.jp>
2011-12-28 17:58:12 +0900

    Change to use $srcdir to install from another directory.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+srcdir=$(dirname $0)
 scmdir=$(pkg-config --variable=uim_scmdir uim)
 pixmapsdir=$(pkg-config --variable=uim_datadir uim)/pixmaps
-cp japan-util.scm japan-util-custom.scm "$scmdir"
-cp pixmaps/japan-util.png pixmaps/japan-util_dark_background.png "$pixmapsdir"
-cp pixmaps/japan-util.svg pixmaps/japan-util_dark_background.svg "$pixmapsdir"
+cp "$srcdir/japan-util.scm" "$srcdir/japan-util-custom.scm" "$scmdir"
+cp "$srcdir/pixmaps/japan-util.png" "$srcdir/pixmaps/japan-util_dark_background.png" "$pixmapsdir"
+cp "$srcdir/pixmaps/japan-util.svg" "$srcdir/pixmaps/japan-util_dark_background.svg" "$pixmapsdir"
 uim-module-manager --register japan-util
